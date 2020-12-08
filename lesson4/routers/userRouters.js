5a8eb9496a1b41e57ac50a8eb3f1fcb00ad8628f
manemin <manemine80@gmail.com>
2020-12-08 09:08:05 +0200

    fix routes

--- lesson4/routers/userRouters.js
+++ lesson4/routers/userRouters.js
@@ -6,7 +6,7 @@ const userMiddleware = require('../middlewares/usermiddleware');
 const userRouter = Router();
 
 userRouter.get('/', userControl.showUsers);
-userRouter.get('/', userMiddleware.isQueryParamValid, userControl.findByParam);
+userRouter.get('/search', userMiddleware.isQueryParamValid, userControl.findByParam);
 userRouter.delete('/:id', userMiddleware.isIdValid, userMiddleware.isUserValid, userControl.deleteUser);
 userRouter.patch('/:id', userMiddleware.isIdValid, userControl.updateUser);
 userRouter.put('/', userMiddleware.isUserValid, userControl.createUser);
